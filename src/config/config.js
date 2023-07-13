6ce78c0d62d4a011bf4ce7b563a560a320e2650f
Muhammad Raihan Wahyu Yuwono <raihanyuwono@hotmail.com>
2023-07-13 21:07:16 +0700

    first setup - intall packages[express, dotenv, sequelize, mysql2] - setup config for sequelize

---
+++ src/config/config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const dotenv = require("dotenv");
+
+dotenv.config({
+    path: path.resolve("../.env"),
+});
+
+const DEV_USERNAME = process.env.DB_USERNAME;
+const DEV_PASSWORD = process.env.DB_PASSWORD;
+const DEV_DATABASE = process.env.DB_DATABASE;
+const DEV_HOST = process.env.DB_HOST;
+
+module.exports = {
+    development: {
+        username: DEV_USERNAME,
+        password: DEV_PASSWORD,
+        database: DEV_DATABASE,
+        host: DEV_HOST,
+        dialect: "mysql",
+    },
+    test: {
+        username: DEV_USERNAME,
+        password: DEV_PASSWORD,
+        database: DEV_DATABASE,
+        host: DEV_HOST,
+        dialect: "mysql",
+    },
+    production: {
+        username: DEV_USERNAME,
+        password: DEV_PASSWORD,
+        database: DEV_DATABASE,
+        host: DEV_HOST,
+        dialect: "mysql",
+    },
+};
